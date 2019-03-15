f4a51437b123e08ba0c9a8bcdf3e94d698d5460d
Thomas Dattilo <tom@dattilo.io>
2019-03-15 15:16:28 -0400

    People Repository Function Addition

--- src/main/java/lingflashbackend/PeopleRepository.java
+++ src/main/java/lingflashbackend/PeopleRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import lingflashbackend.People;
 
 public interface PeopleRepository extends CrudRepository<People, Integer>{
-
+	@Nullable People findByUnameAndPw(String uname, String pw);
+	boolean existsById(Integer id);
 }
