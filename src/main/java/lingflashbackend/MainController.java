3e79438c4259de0666ad480a044bbec1f9d7356f
Thomas Dattilo <tom@dattilo.io>
2019-03-17 22:13:47 -0400

    Test With Get

--- src/main/java/lingflashbackend/MainController.java
+++ src/main/java/lingflashbackend/MainController.java
@@ -3,7 +3,9 @@ package lingflashbackend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParameter;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
@@ -60,6 +62,10 @@ public class MainController {
 			}
 		}
 		return null;
+	}
+	@GetMapping(path="/gbooks")
+	public @ResponseBody Iterable<Book> getBooks(@RequestParameter int uid){
+			return bookRepository.findByOwner(uid);
 	}
 	@PostMapping(path="/chapters")
 	public @ResponseBody Iterable<Chapter> getChapters(@RequestBody String data){
