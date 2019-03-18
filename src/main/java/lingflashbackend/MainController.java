6ce45aa48be41b2265a654382044dba247a2b1d5
Thomas Dattilo <tom@dattilo.io>
2019-03-17 22:15:28 -0400

    Fixed Get Test

--- src/main/java/lingflashbackend/MainController.java
+++ src/main/java/lingflashbackend/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParameter;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
@@ -64,7 +64,7 @@ public class MainController {
 		return null;
 	}
 	@GetMapping(path="/gbooks")
-	public @ResponseBody Iterable<Book> getBooks(@RequestParameter int uid){
+	public @ResponseBody Iterable<Book> getBooks(@RequestParam int uid){
 			return bookRepository.findByOwner(uid);
 	}
 	@PostMapping(path="/chapters")
