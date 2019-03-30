bed7af70026d9addaa99c22f3f04d75a38eb70d2
Thomas Dattilo <tom@dattilo.io>
2019-03-30 18:14:35 -0400

    Correction

--- src/main/java/lingflashbackend/MainController.java
+++ src/main/java/lingflashbackend/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
-import java.util;
+import java.util.List;
 
 import lingflashbackend.People;
 import lingflashbackend.PeopleRepository;
--- src/main/java/lingflashbackend/WordRepository.java
+++ src/main/java/lingflashbackend/WordRepository.java
@@ -2,7 +2,7 @@ package lingflashbackend;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.Nullable;
-import java.util;
+import java.util.List;
 
 import lingflashbackend.Word;
 
